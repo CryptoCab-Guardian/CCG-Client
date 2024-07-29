0224a7a1bd2bd5ba0d7bd16a092aeca83ffb0862
ABHIRUP <abhirupsaha02@gmail.com>
2024-07-30 02:03:33 +0530

    metamask wallet

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import MetaMaskAuth from "@/components/MetaMaskAuth";
+
 export default function Home() {
   return (
-    <div>
-      
+    <div className="min-h-screen flex items-center justify-center bg-cover bg-center bg-no-repeat text-center">
+      <MetaMaskAuth />
     </div>
   )
 }
